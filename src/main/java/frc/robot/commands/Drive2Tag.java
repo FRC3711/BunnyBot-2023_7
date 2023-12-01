ae8e05ebe6e0eb892c40d9ea0ecd5dec3b2e6d13
Trout Lake School First Robotics Team <robotics@tlschool.net>
2023-11-30 17:02:49 -0800

    Drive2Tag initial attempt to impact tote and stop

--- src/main/java/frc/robot/commands/Drive2Tag.java
+++ src/main/java/frc/robot/commands/Drive2Tag.java
@@ -1,16 +1,15 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Drive2Tag uses PhotonVision camera usb2 to locate the AprilTag.
+// It start driveing at the commanded speed (drive value).
+// when it captures tag it steers toward tag and slows down when out
+// about 4 feet.  When it slows to the impact speed, it will continue
+// for 2 seconds.  Hopefully colliding into tote and catching a bunny
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import java.util.List;
-
-import javax.net.ssl.CertPathTrustManagerParameters;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonTrackedTarget;
@@ -24,6 +23,7 @@ public class Drive2Tag extends CommandBase {
   PhotonCamera camera = new PhotonCamera("usb2"); // %rod
   double turnLimit = 0.5;
   private double m_fwdLimit;
+  double startTime;
 
   public Drive2Tag(DriveTrain subsystem, double fwdLimit) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -31,19 +31,20 @@ public class Drive2Tag extends CommandBase {
     addRequirements(m_driveTrain);
 
     m_fwdLimit = fwdLimit;
-
   }
 
   double fwdDrive;
   int fidNumber;
+  double impactDrive = 0.35;  // set this to the desired tote impact speed.
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
 //    camera.setDriverMode(false);
     camera.setPipelineIndex(0); // pipeline 0=bucket
+    startTime = 0;
     fwdDrive = m_fwdLimit; // start at selected speed to start.
-    fidNumber = (int)SmartDashboard.getNumber( "Fiducial #", 11);  // %r6
+    fidNumber = (int)SmartDashboard.getNumber( "Fiducial #", 2);  // %r6
  //   SmartDashboard.pu.putNumber("fid1", fidNumber);
   }
 
@@ -73,33 +74,46 @@ public class Drive2Tag extends CommandBase {
           // if (Math.abs(turnDrive) < 0.1) // if drive is less than 0.1 do nothing
           // turnDrive = 0;
 
-          // the area will indicate how close we are to target. range for .1 to 40.
+          // the area will indicate how close we are to target. range about .1 to 40.
+          // for Arducam uc-844 the area is 7% at 1 foot and 30% at 2 feet
           double area = target.getArea(); // check up/down position
 
-          // the following is a kludge, but it may work.
-          area /= 4; // 15>>10 %r5; // this will reduce drive to 0 at xxx area
-          fwdDrive = 1 - area; // reduce drive to 1/2 if area = 15 / 30
+          // reduce speed as the Tag gets bigger
+          area /= 7; // This will reduce drive to zero at 2 feet.
+          fwdDrive = 1 - area; // reduce drive as we approach 2 feet
+
           if (fwdDrive > m_fwdLimit) // limit the drive to +/- 0.5
             fwdDrive = m_fwdLimit;
-          else if (fwdDrive < 0)  // %r6
-            fwdDrive = 0; // do not go backwards
-          // if (Math.abs(fwdDrive) < 0.35) // if drive is less than 0.1 do nothing
-          //   fwdDrive = 0.35; // creep forward
-
-         }
+          if (Math.abs(fwdDrive) < impactDrive) // if drive is less than impact speed
+          {
+            fwdDrive = impactDrive; // maintain impact speed
+            if (startTime == 0)
+              startTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+          }
+        }
       }
     }
-    m_driveTrain.drive(fwdDrive, turnDrive);
+    // needed the following, since we lose target during impact.
+    if (startTime > 0)
+      m_driveTrain.drive(impactDrive, 0);
+    else
+      m_driveTrain.drive(fwdDrive, turnDrive);
   }
 
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_driveTrain.drive(0,0);  // stop
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if (startTime > 0)  // impact timer initiated
+      if ((edu.wpi.first.wpilibj.Timer.getFPGATimestamp() - startTime) > 2) 
+        return true;  // stop after 2 seconds
+
     return false;
   }
 }
